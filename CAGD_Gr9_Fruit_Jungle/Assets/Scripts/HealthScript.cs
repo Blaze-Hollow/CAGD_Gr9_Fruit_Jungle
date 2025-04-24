4738dd0737376bfaca840fe0dc94f31a2a7da933
SDR1109 <sdr1109v@gmail.com>
2025-04-24 16:56:35 -0700

    Health script added

--- CAGD_Gr9_Fruit_Jungle/Assets/Scripts/HealthScript.cs
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/HealthScript.cs
@@ -1,9 +1,14 @@
+/* Author [Diaz,Samuel]
+ * Last Updated [04/24/2025]
+ * Description [This script is in charge of giving the player health back once they enter in contact with the health pack]
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
+    public int giveHealth = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,16 @@ public class HealthScript : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerController>())
+        {
+            int newHealth = giveHealth + other.GetComponent<PlayerController>().healthPoints;
+            other.GetComponent<PlayerController>().healthPoints = Mathf.Clamp(newHealth, 0, other.GetComponent<PlayerController>
+                ().maxHealthPoints);
+
+            Destroy(gameObject);
+        }
     }
 }
