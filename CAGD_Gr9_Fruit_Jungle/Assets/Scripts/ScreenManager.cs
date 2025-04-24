e3cfd056618de168f3d90d83aba4b300a7a3d194
Blaze-Hollow <blazehollow718@gmail.com>
2025-04-24 14:48:32 -0700

    Folders & scripts
    Created the folders that we will be needing and created two scripts that I will be working on

---
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Andrade, Maya
+ * Created: 04/24/2025
+ * Last Updated: 04/24/2025
+ * Description: This will handle all the scene switching in the game as well as pausing
+ */
+public class ScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Andrade, Maya
+ * Created: 04/24/2025
+ * Last Updated: 04/24/2025
+ * Description: This will handle the UI elements of the game (ex: healthbar and fruit score)
+ */
+public class UI_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
