fbcd77fad55462f87021cb9a332ff2e6362f6150
Blaze-Hollow <blazehollow718@gmail.com>
2025-04-29 16:39:09 -0700

    Durian Started

---
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/DurianEnemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Author: Andrade, Maya
+ * Created: 04/29/2025
+ * Last Updated: 04/29/2025
+ * Description: This will handle the durian enemy movement and attack
+ */
+
+public class DurianEnemy : MonoBehaviour
+{
+    public int speed = 10;
+    public int health = 10;
+    private bool hasExploded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0 && !hasExploded)
+        {
+            Instantiate(DurianSplatter, transform.position);
+        }
+    }
+}
