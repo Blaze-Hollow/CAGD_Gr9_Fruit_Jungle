70523ad5833c26835fc19cb4c77010d5c36211a3
Blaze-Hollow <blazehollow718@gmail.com>
2025-05-08 23:42:49 -0700

    Durian Edits

---
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/DurianSides.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: Andrade, Maya
+ * Created: 05/08/2025
+ * Last Updated: 05/08/2025
+ * Description: This makes the durian damage player if the player collides (not crushed) with the durian
+ */
+public class DurianSides : MonoBehaviour
+{
+    //when the player collides with the side of the durian, it takes 15hp of damage
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().healthPoints -= 15;
+        }
+    }
+}
