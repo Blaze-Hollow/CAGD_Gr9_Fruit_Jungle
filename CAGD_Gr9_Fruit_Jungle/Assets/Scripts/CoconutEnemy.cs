636ae43bb8c194fada990963060c926fa20234ec
SDR1109 <sdr1109v@gmail.com>
2025-05-09 23:09:10 -0700

    some problems with the coconut enemy

--- CAGD_Gr9_Fruit_Jungle/Assets/Scripts/CoconutEnemy.cs
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/CoconutEnemy.cs
@@ -10,19 +10,39 @@ public class CoconutEnemy : MonoBehaviour
 {
     public int Health = 10;
     public bool dropped;
-    public float moveSpeed = 10;
-    Transform target;
+    public float moveSpeed = 10f;
+    Rigidbody rb;
+   public Transform target;
     Vector3 moveDirection;
     public GameObject CoconutDrop;
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+
+
+    {
+        
         target = GameObject.Find("Player").transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(target)
+        {
+            Vector3 direction = (target.position - moveDirection).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+           // rb.rotation = angle;
+            moveDirection = direction;
+
+        }
+
         if (Health <= 0)
         {
 
@@ -34,7 +54,14 @@ public class CoconutEnemy : MonoBehaviour
             Destroy(gameObject);
         }
     }
-   
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z) * moveSpeed;  
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
