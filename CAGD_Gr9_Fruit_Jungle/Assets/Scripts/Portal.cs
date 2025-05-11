56dd0cd6d6c7b1d89672a0eb108a4894b250f2ae
SDR1109 <sdr1109v@gmail.com>
2025-05-11 14:30:11 -0700

    Portal created and advance in the first level

---
+++ CAGD_Gr9_Fruit_Jungle/Assets/Scripts/Portal.cs
@@ -0,0 +1,32 @@
+/* Author [Diaz,Samuel]
+ * Last Updated [04/24/2025]
+ * Description [This script is in charge of changing the levels once the player reaches the portal]
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public int Level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            SceneManager.LoadScene(Level);
+        }
+    }
+}
